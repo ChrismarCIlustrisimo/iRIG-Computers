e3a60ca7e1d454dcc0ec50a8fac71eb768892af8
Chrismar Ilustrisimo <chrismarilustrisimo9@gmail.com>
2024-12-05 00:37:48 +0800

    UPDATE: Locatio website

--- src/onlineListing/pages/StoreLocation.jsx
+++ src/onlineListing/pages/StoreLocation.jsx
@@ -1,123 +1,139 @@
-import Navbar from '../components/Navbar';
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
-import RawIrig from "../assets/RawIrig.jpeg";
-import GMapsIrig from "../assets/GmapsIrig.jpeg";
-import GMapLogo from "../assets/GmapLogo.jpeg";
-import WazeLogo from "../assets/WazeLogo.jpeg";
 import Footer from "../components/Footer";
-import { FaDirections } from "react-icons/fa";
-import { IoSearch } from "react-icons/io5";
+import GMapLogo from "../assets/GmapLogo.jpeg";
+import WazeLogo from "../assets/WazeLogo.png";
+import { BiSolidDownArrow } from "react-icons/bi";
+import { BiSolidUpArrow } from "react-icons/bi";
 
 const StoreLocation = () => {
-    return (
-        <div>
-            <Navbar />
-            <Searchbar />
+    const [isOpen, setIsOpen] = useState(false)
 
-            <div className="mt-32 text-black text-center">
-                <h1 className="text-3xl font-bold">Store Location</h1>
-                <p className="mt-4">
-                    Our knowledgeable staff is always on hand to assist you with any
-                    questions you may have and help you find the perfect solution.
-                </p>
-            </div>
+    const isStoreOpen = () => {
+        const now = new Date();
+        const currentHour = now.getHours();
+    
+        // Check if time is between 9 AM and 6 PMs
+        return currentHour >= 9 && currentHour < 18;
+      };
 
-            <div className="max-w-4xl mx-auto p-6 mt-8 flex flex-col md:flex-row items-center justify-center gap-12">
-            <div className="flex flex-col space-y-6 w-72  my-4">
-                    <div className="flex justify-center items-center space-x-8">
+      const getDayName = () => {
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const now = new Date();
+        return days[now.getDay()];
+      };
 
-                            <div className='flex flex-col items-center justify-center w-full gap-2'>
-                                <div className='w-full  bg-[#007BFF] p-2 rounded-lg flex items-center justify-start gap-2 transform transition-all duration-300 hover:scale-110'>
+  return (
 
-                                    <a 
-                                        href="https://www.google.com/maps/place/iRIG+Computers/@14.6596046,120.9874991,17z/data=!3m1!4b1!4m6!3m5!1s0x3397b682e6ff675d:0xc373eb96f28ced82!8m2!3d14.6596046!4d120.990074!16s%2Fg%2F1jmd0mv4j?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
-                                        target="_blank" 
-                                        rel="noopener noreferrer"
-                                        className='flex items-center justify-center gap-2 text-lg'
-                                        >
-                                        <FaDirections size={30} /> Directions
-                                    </a>
+    <div>
+      <Navbar />
+      <Searchbar />
 
-                                </div>
-                                <div className='w-full  flex items-center justify-start gap-2 p-2 shadow rounded-lg '>
-                                <img
-                                    src={GMapLogo}
-                                    alt="Google Maps"
-                                    className="w-10 h-10"
-                                />
-                                <img
-                                    src={WazeLogo}
-                                    alt="Waze"
-                                    className="w-10 h-10"
-                                />
-                                <p className='text-black font-semibold'>iRIG Computers</p>
-                                </div>
-                            </div>
+      <div className="mt-32 text-black text-center">
+        <h1 className="text-3xl font-bold">Store Location</h1>
+        <p className="mt-4">
+          Our knowledgeable staff is always on hand to assist you with any
+          questions you may have and help you find the perfect solution.
+        </p>
+      </div>
 
+      <div className="max-w-4xl mx-auto p-6 mt-8 flex flex-col items-start justify-center gap-4 bg-light-container shadow-md mb-12 rounded-md">
+      <div className="relative w-full h-[10%]" style={{ paddingBottom: '37.29%' }}>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.935359326679!2d120.98749907577411!3d14.65960977562786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b682e6ff675d%3A0xc373eb96f28ced82!2siRIG%20Computers!5e0!3m2!1sen!2sph!4v1733323747814!5m2!1sen!2sph"
+                width="100%"  // Adjust width to 100%
+                height="100%" // Adjust height to 100%
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className="absolute top-0 left-0 rounded-lg shadow-lg"
+            ></iframe>
+    </div>
+
+
+    <div className="flex items-center justify-start w-full gap-2">
+      <div className="w-full md:w-[25%] bg-light-button bg-opacity-10 text-light-button border border-light-button p-2 rounded-lg flex items-center justify-start gap-2 transform transition-all duration-300 hover:scale-110">
+        <img src={GMapLogo} alt="Google Maps" className="w-8 h-8" />
+        <a
+          href="https://www.google.com/maps/place/iRIG+Computers/@14.6596046,120.990074,17z/data=!3m1!4b1!4m6!3m5!1s0x3397b682e6ff675d:0xc373eb96f28ced82!8m2!3d14.6596046!4d120.990074!16s%2Fg%2F1jmd0mv4j?entry=ttu&g_ep=EgoyMDI0MTIwMS4xIKXMDSoASAFQAw%3D%3D"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center gap-2 text-md"
+        >
+          Directions to Maps
+        </a>
+      </div>
+
+      <a
+        href="https://www.waze.com/en/live-map/directions/ph/ncr/caloocan/irig-computers?to=place.ChIJXWf_5oK2lzMRgu2M8pbrc8M"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-full md:w-[25%] bg-light-button bg-opacity-10 text-light-button border border-light-button flex items-center justify-start gap-2 p-2 shadow rounded-lg transform transition-all duration-300 hover:scale-110"
+      >
+        <img src={WazeLogo} alt="Waze" className="w-8 h-8" />
+        iRIG Computers
+      </a>
+    </div>
+
+
+        <div className="flex flex-col item-center justify-start text-black gap-4">
+            <p><span className="font-bold mr-2">Address: </span>23 General Tinio, Morning Breeze Subdivision, Caloocan, 1401 Metro Manila</p>
+
+            <div className="flex flex-col sm:flex-row gap-2 items-start justify-start">
+            <p className="font-bold mr-2">Store Hours: </p>
+                <div className="flex gap-2 items-center w-full md:w-[50%]" onClick={() => setIsOpen(!isOpen)}>
+                  {isOpen ? (
+                    <>
+                      <div>{isStoreOpen() ? <p className="text-green-600 font-semibold">Open</p> : <p className="text-red-600 font-semibold">Closed</p>}</div>
+                      <p>Opens 9AM {getDayName()}</p>
+                      <BiSolidDownArrow size={10} />
+                    </>
+                  ) : (
+                    <div className="flex flex-col w-full gap-4 font-base">
+                      <div className="flex items-center justify-between w-full sm:w-[68%]">
+                        <p>Monday</p>
+                        <div className="flex items-center justify-center gap-4">9 AM - 6 PM <BiSolidUpArrow size={10} /></div>
+                      </div>
+                      <div className="flex items-center justify-between w-full sm:w-[60%]">
+                        <p>Tuesday</p>
+                        <p>9 AM - 6 PM</p>
+                      </div>
+                      <div className="flex items-center justify-between w-full sm:w-[60%]">
+                        <p>Wednesday</p>
+                        <p>9 AM - 6 PM</p>
+                      </div>
+                      <div className="flex items-center justify-between w-full sm:w-[60%]">
+                        <p>Thursday</p>
+                        <p>9 AM - 6 PM</p>
+                      </div>
+                      <div className="flex items-center justify-between w-full sm:w-[60%]">
+                        <p>Friday</p>
+                        <p>9 AM - 6 PM</p>
+                      </div>
+                      <div className="flex items-center justify-between w-full sm:w-[60%]">
+                        <p>Saturday</p>
+                        <p>9 AM - 6 PM</p>
+                      </div>
+                      <div className="flex items-center justify-between w-full sm:w-[60%]">
+                        <p>Sunday</p>
+                        <p>Closed</p>
+                      </div>
                     </div>
-
-                    <div className="flex items-center justify-between text-black">
-                        <h1 className="text-xl font-bold">iRIG Computers</h1>
-                    </div>
-
-                    <div className="text-lg text-black bg-white rounded-lg shadow px-3 py-4">
-                        <p>23 General Tinio, Morning Breeze Subdivision,</p>
-                        <p>Caloocan, 1401 Metro Manila</p>
-                    </div>
-
-                    <div className="text-lg text-black bg-white rounded-lg shadow px-3 py-4">
-                        <div className="font-medium text-red-500">Store Hours:</div>
-                        <div className="flex justify-between ">
-                            <p>Mon - Sat:</p>
-                            <p className="w-28 text-left">9AM - 6PM</p>
-                        </div>
-                        <div className="flex justify-between ">
-                            <p>Sunday:</p>
-                            <p className="w-28 text-left">Closed</p>
-                        </div>
-                    </div>
-
-                    <div className="text-lg text-black bg-white rounded-lg shadow px-3 py-4">
-                        <div className="font-medium text-red-500">Contact:</div>
-                        <p>0923-444-1030</p>
-                        <p>(02) 83646039</p>
-                    </div>
+                  )}
                 </div>
 
-                <div className="grid grid-cols-1 gap-6 w-full md:w-[58%]">
-                    {/* Storefront Image */}
-                    <a
-                        href="https://www.google.com/maps/uv?pb=!1s0x3397b682e6ff675d%3A0xc373eb96f28ced82!3m1!7e115!4s%2Fmaps%2Fplace%2F23%2BGeneral%2BTinio%2C%2BMorning%2BBreeze%2BSubdivision%2C%2BCaloocan%2C%2B1401%2BMetro%2BManila%2BIrig%2BComputers%2F%4014.6596121%2C120.9901217%2C3a%2C75y%2C260.8h%2C90t%2Fdata%3D*213m4*211e1*213m2*211sRlmAJq2tW2U19gMEdd720Q*212e0*214m2*213m1*211s0x3397b682e6ff675d%3A0xc373eb96f28ced82%3Fsa%3DX%26ved%3D2ahUKEwi7goSNreGJAxXsna8BHbjqMisQpx96BAg6EAA!5s23%20General%20Tinio%2C%20Morning%20Breeze%2BSubdivision%2C%2BCaloocan%2C%201401%20Metro%20Manila%2BIrig%20Computers%20-%20Google%20Search!15sCgIgAQ&imagekey=!1e2!2sRlmAJq2tW2U19gMEdd720Q&cr=le_a7&hl=en&ved=1t%3A206134&ictx=111"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="rounded-lg overflow-hidden shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
-                    >
-                        <img
-                            src={RawIrig}
-                            alt="Storefront"
-                            className="w-full h-full object-cover"
-                        />
-                    </a>
-
-                    {/* Map Image */}
-                    <a
-                        href="https://www.google.com/maps/place/iRIG+Computers/@14.6596046,120.9874991,17z/data=!3m1!4b1!4m6!3m5!1s0x3397b682e6ff675d:0xc373eb96f28ced82!8m2!3d14.6596046!4d120.990074!16s%2Fg%2F1jmd0mv4j?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="rounded-lg overflow-hidden shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
-                    >
-                        <img
-                            src={GMapsIrig}
-                            alt="Map"
-                            className="w-full h-full object-cover"
-                        />
-                    </a>
-                </div>
             </div>
 
-            <Footer />
-        </div>
-    );
+            <p><span className="font-bold mr-2">Contact: </span>(02) 83646039 | 0923-444-1030</p>
+            </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
+
 
 export default StoreLocation;
